423b87d2561a8d9acf6366a03ddc44ca094f3ba1
Dipsikha Banerjee <dipsikhabanerjee1999@gmail.com>
2021-07-05 20:49:15 +0530

    d commit

--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -3,11 +3,11 @@ import './style.css';
 const Navbar = () => {
 return (
 	<nav>
-		<a href="/home"><img src="./favicon.png"></img></a>
+		<a href="/"><img src="./favicon.png"></img></a>
 		<ul>
 			<li><a href="/">Home</a></li>
 			<li><a href="/about">About</a></li>
-			<li><a href="/contacts">Always Welcome</a></li>
+			<li><a href="/contacts">Contacts</a></li>
 			<li><a href="/blogs">Blogs</a></li>
 		</ul>
 	</nav>
--- my-app/src/containers/home/index.js
+++ my-app/src/containers/home/index.js
@@ -1,32 +1,48 @@
-import React from 'react';
-import Typed from 'react-typed';
-import './style.css';
+import React from "react";
+import Typed from "react-typed";
+import "./style.css";
 
-const Home  = props => {
-    return (
-        <div className="main">
+const Home = (props) => {
+  return (
+    <div className="main">
+      <div id="home">
+        <h1>Change the world by being yourself</h1>
+        <h3 id="type">
+          <Typed
+            className="text-type"
+            strings={[
+              "Welcome to Tyrant blogging",
+              "Love For All, Hatred For None.",
+              "Die with memories, not dreams",
+              "Aspire to inspire before we expire",
+            ]}
+            typeSpeed={50}
+            backSpeed={50}
+            loop
+          />
+        </h3>
+      </div>
 
-            <div id="home">
-                <h1>Change the world by being yourself</h1>
-                <h3 id="type">
-                <Typed className="text-type"
-                    strings={["Welcome to Tyrant blogging",
-                    "Love For All, Hatred For None.",
-                    "Die with memories, not dreams",
-                    "Aspire to inspire before we expire"]}
-                    typeSpeed={50}
-                    backSpeed={50}
-                    loop
-                />
-                </h3>
-            </div>
-
-            <div id="testimonials">
-                <h1>Testimonials</h1>
-            </div>
-            
+      <div id="testimonials">
+        <h1>Testimonials</h1>
+        <div className="container">
+          <img src="bandmember.jpg" alt="Avatar" />
+          <p>
+            <span>Chris Fox.</span> CEO at Mighty Schools.
+          </p>
+          <p>John Doe saved us from a web disaster.</p>
         </div>
-      );
-}
+
+        <div className="container">
+          <img src="avatar_g2.jpg" alt="Avatar" />
+          <p>
+            <span>Rebecca Flex.</span> CEO at Company.
+          </p>
+          <p>No one is better than John Doe.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
