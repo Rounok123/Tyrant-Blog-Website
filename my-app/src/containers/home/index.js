a133689884077a64409b94065af18f70e41f2527
Rounok Das <rounokdasrkd@gmail.com>
2021-07-05 17:59:42 +0530

    r1 commit

--- my-app/src/containers/home/index.js
+++ my-app/src/containers/home/index.js
@@ -4,20 +4,47 @@ import './style.css';
 
 const Home  = props => {
     return (
-        <div id="home">
-            <h1>Welcome to Tyrant Bloging</h1>
-            <h3 id="type">
-            <Typed className="text-type"
-                strings={["uhgiuer","isd,ogfgf","hlcshcks"]}
-                typeSpeed={40}
-                backSpeed={60}
-                loop
-            />
-            </h3>
+        <div className="main">
 
-            {/* <h2><a href="#" id="btn">Go To Blog</a></h2> */}
+            <div id="home">
+                <h1>Change the world by being yourself</h1>
+                <h3 id="type">
+                <Typed className="text-type"
+                    strings={["Welcome to Tyrant blogging",
+                    "Love For All, Hatred For None.",
+                    "Die with memories, not dreams",
+                    "Aspire to inspire before we expire"]}
+                    typeSpeed={50}
+                    backSpeed={50}
+                    loop
+                />
+                </h3>
+            </div>
+
+            <div id="testimonials">
+                <h1>Testimonials</h1>
+            </div>
+            {
+                $('.testi3').owlCarousel({
+                    loop: true,
+                    margin: 30,
+                    nav: false,
+                    dots: true,
+                    autoplay: true,
+                    responsiveClass: true,
+                    responsive: {
+                      0: {
+                        items: 1,
+                        nav: false
+                      },
+                      1024: {
+                        items: 3
+                      }
+                    }
+                  })
+            }
         </div>
-    );
+      );
 }
 
 export default Home;
